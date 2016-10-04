d0bc24f3e1a9f462858254d01cb4bdfd4ad01313
EtienneDh <etdhennin@gmail.com>
2016-10-04 14:56:13 +0200

    did round fonctions with delay, wrote a couple of functions for refactoring purpose

--- js/main.js
+++ js/main.js
@@ -1,191 +1,215 @@
-/* Ready To Rumble */
+/*Ready To Rumble*/
 
-$(document).ready(function() {
+// Figther constructor
+function Fighter(name, health, damagePerAttack, special, avatar) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttack = damagePerAttack;
+    this.special = special;
+    this.avatar = avatar;
+    this.toString = function() {
+        return this.name;
+    };
+}
 
-    // Fighter object constructor
-    function Fighter(name, health, damagePerAttack, special, avatar) {
-        this.name = name;
-        this.health = health;
-        this.damagePerAttack = damagePerAttack;
-        this.special = special;
-        this.avatar = avatar;
-        this.toString = function() {
-            return this.name;
-        }
-    }
+// Set fighters
+fightersFunctionSet = function() {
+    ryan = new Fighter('Ryan', 110, 24, 'Triple Punch', 'img/Ryan.png');
+    xavier = new Fighter('Xavier', 100, 14, 'Troll Attack', 'img/Ryan.png');
+    aaron = new Fighter('Aaron', 145, 17, 'Black Mamba', 'img/Aaron.png');
+    totor = new Fighter('Totor', 90, 14, 'Equarissage', 'img/Ryan.png');
+    crispy = new Fighter('Crispy', 100, 16, 'Axe Throw', 'img/Crispy.png');
+    chuck = new Fighter('Chuck Norris', 9999, 99, 'Round House Kick', 'img/Ryan.png');
 
-    // Create fighters
-    var ryan = new Fighter('Ryan', 110, 24, 'Triple Punch', 'img/Ryan.png');
-    var xavier = new Fighter('Xavier', 100, 14, 'Troll Attack', 'img/Ryan.png');
-    var aaron = new Fighter('Aaron', 145, 17, 'Black Mamba', 'img/Aaron.png');
-    var totor = new Fighter('Totor', 90, 14, 'Equarissage', 'img/Ryan.png');
-    var crispy = new Fighter('Crispy', 100, 16, 'Axe Throw', 'img/Crispy.png');
-    var chuck = new Fighter('Chuck Norris', 9999, 99, 'Round House Kick', 'img/Ryan.png');
-
-    //Global variable
     fightersArray = [ryan, aaron, totor, chuck, xavier, crispy];
+};
 
-    // Create the list of fighters for user to select
-    function fightersList(fightersArray) {
-        var $select = $('.fighter-list');
-        for (var fighter = 0; fighter < fightersArray.length; fighter++) {
-            $option = $('<option></option>');
-            $option.attr('value', fightersArray[fighter].name.toLowerCase());
-            $option.append(fightersArray[fighter].name);
-            $select.append($option);
-        }
-    }
-    // End document ready
+// round global var
+roundNum = 0;
 
-    // Draw Game Window Function
-    function drawWindow() {
-        var $player1Div = $('<div id="player1-Div"><h2 id="player1-name">Player 1</h2><p id="player1-hp">0 HP</div>');
-        var $player2Div = $('<div id="player2-Div"><h2 id="player2-name">Player 2</h2><p id="player2-hp">0 HP</div>');
-        var $outputDiv = $('<div id="outputDiv"></div>')
-            //Display
-        $('.game-window').append($player1Div);
-        $('.game-window').append('<div class="middle"><h3>VS</h3><h4>FIGHT !</h4>');
-        $('.game-window').append($player2Div);
-        $('.output').append($outputDiv);
-    }
+/******************************************************************************/
 
-    fightersList(fightersArray.sort());
-    drawWindow();
+// DOM elements
 
-});
+domElementsFunctionSet = function() {
+    /* Player Div's */
+    $player1Div = $('<div id="player1-Div"></div>');
+    $player2Div = $('<div id="player2-Div"></div>');
 
-// Get the selected players and refresh window with their information
+    /* Elements for players' Name, HPs & Avatars */
+    $player1NameDisplay = $('<h2 id="player1-name"></h2>');
+    $player2NameDisplay = $('<h2 id="player2-name"></h2>');
 
-function run() {
-    // Get the value of selected elements (fighters)
-    var player1 = document.getElementById("f-l-1").value;
-    var player2 = document.getElementById("f-l-2").value;
+    $player1HpDisplay = $('<p id="player1-hp"></p>');
+    $player2HpDisplay = $('<p id="player2-hp"></p>');
 
-    // match value with corresponding objects
-    for (var i = 0; i < fightersArray.length; i++) {
-        if (fightersArray[i].name.toLowerCase() == player1) {
-            var assignPlayer1 = fightersArray[i];
-        }
-        if (fightersArray[i].name.toLowerCase() == player2) {
-            var assignPlayer2 = fightersArray[i];
-        }
-    }
-    // Global Variable
-    playersArray = [assignPlayer1, assignPlayer2];
+    $player1ADisplay = $('<img class="avatar" id="player1-avatar">');
+    $player2ADisplay = $('<img class="avatar" id="player2-avatar">');
 
-    refreshWindow(assignPlayer1, assignPlayer2);
-}
+    /* Misc */
+    $outputDiv = $('<div id="outputDiv"></div>'); // Output battle info Div
+    $versusDiv = $('<div class="middle"><h3>VS</h3><h4 class="fight" onclick="fight()">FIGHT !</h4>'); // Middle Div
+};
 
-// Return array of selected players
-function getPlayers() {
-    return playersArray;
-}
+
+/******************************************************************************/
+
+// Game Functions
+
+// Interface builder & refresher
+refreshDomElement = function(domElement, modification) {
+    return domElement.text(modification);
+};
+
+// Print result of rounds
+printMsg = function(attacker, defenser) {
+  return attacker.name + ' hits ' + defenser.name + ' for ' + "<span id='hitdmg'>" + attacker.damagePerAttack + '</span>' + ' damages \!';
+};
+
+// Keep $outputDiv at bottom
+stickToBottom = function() {
+  var element = document.getElementById('outputDiv');
+  element.scrollTop = element.scrollHeight;
+};
 
 // Get a random number from interval
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-/*Refresh window*/
+// Generate list of playable fighters
+function fightersList(fightersArray) {
+    var $select = $('.fighter-list');
 
-function refreshWindow(player1, player2) {
-
-    // Remove every children of game window to refresh displayed data
-    $('.game-window').empty();
-
-    // Create 1 div per player
-    var $player1Div = $('<div id="player1-Div"></div>');
-    var $player2Div = $('<div id="player2-Div"></div>');
-
-    // Create html elements to display players' info
-
-    /*name*/
-    var $player1NameDisplay = $('<h2 id="player1-name"></h2>');
-    var $player2NameDisplay = $('<h2 id="player2-name"></h2>');
-    /*hp*/
-    var $player1HpDisplay = $('<p id="player1-hp"></p>');
-    var $player2HpDisplay = $('<p id="player2-hp"></p>');
-    /*Avatar*/
-    var $player1ADisplay = $('<img class="avatar" id="player1-avatar">');
-    var $player2ADisplay = $('<img class="avatar" id="player2-avatar">');
-
-    // Add content
-
-    /*name*/
-    $player1NameDisplay.append(player1.name);
-    $player2NameDisplay.append(player2.name);
-
-    /*hp*/
-    $player1HpDisplay.append(player1.health + ' hp');
-    $player2HpDisplay.append(player2.health + ' hp');
-
-    /*Avatar*/
-    $player1ADisplay.attr('src', player1.avatar);
-    $player2ADisplay.attr('src', player2.avatar);
-
-    // append
-
-    /*Player 1*/
-    $player1Div.append($player1NameDisplay);
-    $player1Div.append($player1ADisplay)
-    $player1Div.append($player1HpDisplay);
-    /*Player 2*/
-    $player2Div.append($player2NameDisplay);
-    $player2Div.append($player2ADisplay)
-    $player2Div.append($player2HpDisplay);
-
-    //Display
-    $('.game-window').append($player1Div);
-    $('.game-window').append('<div class="middle"><h3>VS</h3><h4 class="fight" onclick="fight()">FIGHT !</h4>');
-    $('.game-window').append($player2Div);
-
+    for (var fighter = 0; fighter < fightersArray.length; fighter++) {
+        var $option = $('<option></option>');
+        $option.attr('value', fightersArray[fighter].name.toLowerCase());
+        $option.append(fightersArray[fighter].name);
+        $select.append($option);
+    }
 }
 
+// Window builder
+windowBuilder = function() {
+
+    /* Player 1 */
+    $player1Div.append(refreshDomElement($player1NameDisplay, 'Player 1'));
+    $player1Div.append($player1ADisplay);
+    $player1Div.append(refreshDomElement($player1HpDisplay, 'HP: 0'));
+
+    /* Player 2 */
+    $player2Div.append(refreshDomElement($player2NameDisplay, 'Player 2'));
+    $player2Div.append($player2ADisplay);
+    $player2Div.append(refreshDomElement($player2HpDisplay, 'HP: 0'));
+
+    // Display on screen
+    $('.game-window').append($player1Div);
+    $('.game-window').append($versusDiv);
+    $('.game-window').append($player2Div);
+    $('.output').append($outputDiv);
+
+    fightersList(fightersArray.sort());
+};
+
+
+// The function run loads selected fighters
+function run() {
+    // Get selected fighter by user
+    var player1 = document.getElementById("f-l-1").value;
+    var player2 = document.getElementById("f-l-2").value;
+
+    // match value with corresponding objects (fighters) & refresh elements with players' info
+    for (var i = 0; i < fightersArray.length; i++) {
+        if (fightersArray[i].name.toLowerCase() == player1) {
+            refreshDomElement($player1NameDisplay, fightersArray[i].name);
+            refreshDomElement($player1HpDisplay, 'HP: ' + fightersArray[i].health);
+            $player1ADisplay.attr('src', fightersArray[i].avatar);
+            // Clone player 1
+            var assignPlayer1 = $.extend(true, {}, fightersArray[i]);
+
+        }
+        if (fightersArray[i].name.toLowerCase() == player2) {
+            refreshDomElement($player2NameDisplay, fightersArray[i].name);
+            refreshDomElement($player2HpDisplay, 'HP: ' + fightersArray[i].health);
+            $player2ADisplay.attr('src', fightersArray[i].avatar);
+            // Clone player 2
+            var assignPlayer2 = $.extend(true, {}, fightersArray[i]);
+        }
+    }
+    // Global array containing fighters
+    playersArray = [assignPlayer1, assignPlayer2];
+}
+
+// The function Fight initiates the game
 function fight() {
-    getPlayers();
+    alert('Fight Motherfucker!');
+    // Update interface with fighters' name and hp's
+    refreshDomElement($outputDiv, playersArray[0].name + ' and ' + playersArray[1].name + ' are going to fight !');
+
+    var first = randomIntFromInterval(1, 2);
     player1 = playersArray[0];
     player2 = playersArray[1];
 
-    // Define variables
-    var roundFighter;
-    var roundDefencer;
-    var round = 0;
-    var first = randomIntFromInterval(1, 2);
-
-    // Choose first attacker
+    // Choose first attacker and add new property on chosen player
     if (first === 1) {
-        roundFighter = player1;
+        player1.start = 1;
     } else {
-        roundFighter = player2;
+        player2.start = 1;
+    }
+    // Round function
+    round();
+}
+
+
+// The function Round() resolves each fighting round, print results and call itself with a delay. Stop when 1 players dies
+function round() {
+    var txt = '';
+    // Check for players life
+    if (player1.health <= 0 || player2.health <= 0) {
+        clearTimeout(timer);
+        alert('fight over');
     }
 
-    // Display Intro
-    $('#outputDiv').empty();
-    var $intro = $('<p class="intro"></p>');
-    var introTxt = 'Welcome To Ready To Rumble Simplon Edition ! ' + player1.name + ' and ' + player2.name + ' are gonna fight to death!';
-    $intro.append(introTxt);
-    $('#outputDiv').append(introTxt);
-
-    //Battle Round
-    setInterval(function() {
-            while (player1.health > 0 && player2.health > 0) {
-                if(round % 2 === 0 && round !== 0) {
-                  roundFighter = player1;
-                  roundDefencer = player2;
-                  alert('test1');
-                } else if (round % 2 !== 0) {
-                  roundFighter = player2;
-                  roundFighter = player1;
-                  alert('test2');
-                } else {
-                  roundDefencer = player2;
-                  alert('test1');
-                }
-                player1.health -= 35;
-                round++;
+    roundFight = function() {
+        // Check round number to define attackers/ defenser
+        if (roundNum % 2 === 0) {
+            if (player1.hasOwnProperty('start')) {
+                txt = printMsg(player1, player2);
+                player2.health -= player1.damagePerAttack;
+                refreshDomElement($player2HpDisplay, player2.health);
+            } else {
+                txt = printMsg(player2, player1);
+                player1.health -= player2.damagePerAttack;
+                refreshDomElement($player1HpDisplay, player1.health);
             }
-    }, 4000);
-
-
-
+        } else {
+            if (player1.hasOwnProperty('start')) {
+                txt = printMsg(player2, player1);
+                player1.health -= player2.damagePerAttack;
+                refreshDomElement($player1HpDisplay, player1.health);
+            } else {
+                txt = printMsg(player1, player2);
+                player2.health -= player1.damagePerAttack;
+                refreshDomElement($player2HpDisplay, player2.health);
+            }
+        }
+        $outputDiv.append('<br>' + txt);
+        stickToBottom();
+        // Indent round
+        roundNum++;
+        // call round
+        round();
+    };
+    timer = setTimeout(roundFight, 2500);
 }
+
+
+// On load
+$(document).ready(function() {
+    // Load Dom Elements data
+    domElementsFunctionSet();
+    // Load Fighters data
+    fightersFunctionSet();
+    // Build Game Interface
+    windowBuilder();
+});
